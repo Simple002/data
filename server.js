13e41eebe7869bfe895d8e841a9641f6a4e4d312
Simple002 <meyrzhan04.04@mail.ru>
2025-07-16 14:34:01 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require("express")
+const { default: mongoose } = require("mongoose")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const cookieParser = require("cookie-parser")
+const { type } = require("os")
+const { name } = require("ejs")
+require("dotenv").config()
+const app = express()
+
+app.use(express.json())
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+mongoose.connect(process.env.DATABASE)
+.then(() => console.log("MongoDB connected..."))
+.catch((err) => console.log("ERROR..."))
+
+const userSchema = mongoose.Schema(
+    {
+        name:{
+            type:String,
+            unique:true,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    }
+)
+
+const User = mongoose.model("User",userSchema)
+
+//-----------------------------------------------------------------------
+
+async function Auth(req,res,next){
+    const auth_token = req.cookies.token
+    if(!auth_token){return res.redirect('/login')}
+
+    try{
+        const decoded = jwt.verify(auth_token,process.env.SECRET_KEY)
+        req.user = decoded
+        next()
+    }catch{
+        res.redirect('/login')
+    }
+}
+
+//--------------------------------------------------------------------------------------
+
+app.get('/',(req,res)=>{
+    res.render("regis")
+})
+
+app.get('/login',async(req,res)=>{
+    res.render('login')
+})
+
+app.get('/home',async(req,res)=>{
+    res.render('home')
+})
+
+app.get('/profile',Auth,(req,res)=>{
+    res.render('profile',{name: req.user.name})
+})
+
+app.get('/logout',(req,res)=>{
+    res.clearCookie("token")
+    res.json({logOut_status:true})
+})
+
+//--------------------------------------------------------------------------------------
+
+app.get('/check',async(req,res)=>{
+    const cookie = req.cookies.token;
+    if(!cookie){return res.json({status:false})}
+
+    try{
+        const valid = jwt.verify(cookie,process.env.SECRET_KEY)
+        res.json({status:true})
+    }catch{
+        res.json({status:false})
+    }
+})
+
+app.post("/create",async (req,res)=>{
+    const { username,password } = req.body;
+    const find_user = await User.findOne({name:username})
+    if(find_user){
+        return res.status(401).json({status:false})
+    }
+    const token = jwt.sign({name:username},process.env.SECRET_KEY,{expiresIn:"7d"})
+    const hash = await bcrypt.hash(password,10)
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        maxAge:36000000
+    })
+
+    const user = new User({name:username,password:hash})
+    await user.save()
+    res.redirect('/home')
+})
+
+app.post("/logIn",async (req,res)=>{
+    const { username,password } = req.body;
+    const cookie = req.cookies.token
+
+    const find_user = await User.findOne({ name:username })
+    if(!find_user){return res.status(401).json({status:false})}
+
+    const check_hash = await bcrypt.compare(password,find_user.password)
+    if(!check_hash){return res.status(401).json({status:false})}
+    
+    const token = jwt.sign({name:username},process.env.SECRET_KEY,{expiresIn:"7d"})
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 36000000
+    });
+
+    res.redirect('/home')
+})
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log("Server start work..."))
